aec2be3d0f378536673a6ffbcb8e8fe4c2aaebdf
Masher-111 <90869108+Masher-111@users.noreply.github.com>
2023-12-03 10:30:21 +0300

    requests done

--- index.js
+++ index.js
@@ -6,8 +6,22 @@ const users = [];
 
 let uniqueID = 0;
 app.use(express.json())
+
 app.get('/users', (req, res) => {
     res.send({ users })
+})
+
+app.get('/users/:id', (req, res) => {
+    const userID = +req.params.id;
+
+    const user = users.find((user) => user.id === userID);
+
+    if(user) {       
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({user: null});
+    }
 })
 
 app.post('/users', (req, res) => {
@@ -20,4 +34,39 @@ app.post('/users', (req, res) => {
     res.send({ id: uniqueID })
 })
 
+
+app.put('/users/:id', (req, res) =>{
+    const userID = +req.params.id;
+
+    const user = users.find((user) => user.id === userID);
+
+    if(user) {
+        const {firstName, secondName, age, city} = req.body;
+        user.firstName = firstName;
+        user.secondName = secondName;
+        user.age = age;
+        user.city = city;
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({user: null});
+    }    
+})
+
+app.delete('/users/:id', (req, res) =>{
+    const userID = +req.params.id;
+
+    const user = users.find((user) => user.id === userID);
+
+    if(user) {
+        const userIndex = users.indexOf(user);
+        users.splice(userIndex, 1);
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({user: null});
+    }
+
+    
+})
 app.listen(3000);
